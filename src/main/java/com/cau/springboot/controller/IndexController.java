eeabd06219c5b15e4743e3883adbccf17cdadc3e
WuZzz1 <653767813@qq.com>
2021-06-18 10:18:16 +0800

    吴彦祖好帅

--- src/main/java/com/cau/springboot/controller/IndexController.java
+++ src/main/java/com/cau/springboot/controller/IndexController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class IndexController {
-    @RequestMapping(value = "/say2")
+    @RequestMapping(value = "/springboot/say")
     public @ResponseBody String say(){
-        return "吴彦祖真帅";
+        return "Hello SpringBoot!"+" Hello GitHub！"+" Hello 吴彦祖！";
     }
+
 }
